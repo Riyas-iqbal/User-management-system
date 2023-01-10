05927ab50c0cebde3df6dc428f8a29cbfb82c0b4
Riyas Iqbal <riyasiqbal10@gmail.com>
2023-01-10 21:09:48 +0530

    404 Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -36,7 +36,7 @@ function App() {
           <Route path='/login' element={<Login />} />
           <Route path='/notfound' element={<ErrorPage />} />
           <Route path='/admin' element={<AdminLogin/>}></Route>
-          {/* <Route path='/*' element={<ErrorPage/>}/> */}
+          <Route path='/*' element={<ErrorPage/>}/>
         </Routes>
 
         <Footer />
